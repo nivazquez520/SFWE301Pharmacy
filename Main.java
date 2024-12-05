b796433e83a1f4194139c71c2ddfba293ada6080
LimnePower <87224719+LimnePower@users.noreply.github.com>
2024-12-05 12:34:44 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,10 +4,15 @@ public class Main {
     public static void main(String[] args) {
         ReportGenerationFolder.Reports report = new ReportGenerationFolder.Reports();
 
+        InventoryControlFolder.InventoryManagement inventory = new InventoryControlFolder.InventoryManagement();
+        InventoryControlFolder.Inventory inv = new InventoryControlFolder.Inventory();
 
         // testing Report Generation Menu
         report.displayReportMenu();
 
-
+        inventory.EmployeeCreate();
+        inventory.InventorySystem();
+        inventory.InventoryManage(inv);
+        inventory.SimulateExpire(inv);
     }
 }
